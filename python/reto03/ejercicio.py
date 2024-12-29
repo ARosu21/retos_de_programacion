41ff5cab763e77a599ce77ab7429112dbe1d2a1b
Adrian Rosu <arosu4d@gmail.com>
2024-12-29 21:13:42 +0100

    [feat]: Creado reto03

---
+++ python/reto03/ejercicio.py
@@ -0,0 +1,17 @@
+# Estructuras de datos en Python
+
+# Listas:
+# Creación
+my_list = ['Deadpool', 'Wolverine']
+# Inserción
+my_list.append('Thor')
+print(my_list)
+# Borrado
+my_list.remove('Thor')
+print(my_list)
+# Actualización
+my_list[0] = 'Lifepool'
+print(my_list)
+# Ordenación
+my_list.sort()
+print(my_list)
